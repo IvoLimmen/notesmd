becc070034c869ef1cde665a3808b14708677e9d
Ivo Limmen <ivo@limmen.org>
2025-12-18 12:58:15 +0100

    E key now only works on view page.

--- web/main.js
+++ web/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function (event) {
-    if(event.key == 'e') {
+    if(event.key == 'e' && window.location.toString().indexOf("/view/") != -1) {
         var location = window.location.toString().replace("/view/", "/edit/")
         window.location = location;
     }
