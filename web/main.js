eeb36b0ccd32caa3efc2ec3cdb92d95a15de3dd5
Ivo Limmen <ilimmen@rotterdam.nl>
2026-06-12 11:04:33 +0200

    Added a clipboard function to all code blocks.

--- web/main.js
+++ web/main.js
@@ -1,14 +1,22 @@
-document.addEventListener('keydown', function (event) {
-    if(event.key == 'e' && window.location.toString().indexOf("/view/") != -1) {
-        if (document.activeElement != null && document.activeElement.localName === 'input') {
-            return;
+window.addEventListener('DOMContentLoaded',function () {
+    document.addEventListener('keydown', function (event) {
+        if(event.key == 'e' && window.location.toString().indexOf("/view/") != -1) {
+            if (document.activeElement != null && document.activeElement.localName === 'input') {
+                return;
+            }
+            var location = window.location.toString().replace("/view/", "/edit/")
+            window.location = location;
         }
-        var location = window.location.toString().replace("/view/", "/edit/")
-        window.location = location;
-    }
 
-    if(event.ctrlKey && event.key == 's' && window.location.toString().indexOf("/edit/") != -1) {
-        event.preventDefault();
-        document.forms[0].submit()
-    }
+        if(event.ctrlKey && event.key == 's' && window.location.toString().indexOf("/edit/") != -1) {
+            event.preventDefault();
+            document.forms[0].submit()
+        }
+    });
+
+    document.querySelectorAll('pre code').forEach((block) => {
+        block.addEventListener("click", () => {
+            navigator.clipboard.writeText(block.textContent);
+        });
+    });
 });
