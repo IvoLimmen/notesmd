391ea37a667b17b7f06be081b382db4aac4290aa
Ivo Limmen <ivo@limmen.org>
2026-06-22 21:12:26 +0200

    Small fix to make key bindings work correct.

--- web/main.js
+++ web/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded',function () {
     document.addEventListener('keydown', function (event) {
+
         if(event.key == 'e' && window.location.toString().indexOf("/view/") != -1) {
             if (document.activeElement != null && document.activeElement.localName === 'input') {
                 return;
@@ -13,18 +14,21 @@ window.addEventListener('DOMContentLoaded',function () {
             document.forms[1].submit();
         }
 
-        if(event.key == '/') {
-            event.stopPropagation();
-            event.preventDefault();            
-            document.getElementsByName('search')[0].focus();
-        }
-        if(event.key == 'h') {
-            event.stopPropagation();
-            event.preventDefault();            
-            var location = window.location.toString();
-            var index = location.lastIndexOf('/');            
-            var newloc = location.substring(0, index) + 'Index';
-            window.location = newloc;
+        if (document.activeElement != null && document.activeElement.tagName != 'INPUT' && document.activeElement.tagName != 'TEXTAREA') {
+            if(event.key == '/') {
+                event.stopPropagation();
+                event.preventDefault();            
+                document.getElementsByName('search')[0].focus();
+            }
+
+            if(event.key == 'h') {
+                event.stopPropagation();
+                event.preventDefault();            
+                var location = window.location.toString();
+                var index = location.lastIndexOf('/');            
+                var newloc = location.substring(0, index) + 'Index';
+                window.location = newloc;
+            }
         }
     });
 
